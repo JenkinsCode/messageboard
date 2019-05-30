1faa8f88c5073d74ff5f57c54535c50e47ba86aa
JenkinsCode <robertjenkins84@gmail.com>
2019-05-30 09:18:25 -0700

    Added comment section and am able to show comments submitted

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :messages
+  resources :messages do
+    resources :comments
+  end
   root 'messages#index'
 end
   # resources sets up our urls. We want the index root to be nothing right now
